9eeecc025f9ee9c86c3d280f10b81d5e1c311b10
erimerturk <erimerturk@gmail.com>
2023-05-20 22:36:33 +0300

    initial

---
+++ src/main/java/com/bnpl/edgeservice/BnplEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bnpl.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnplEdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnplEdgeServiceApplication.class, args);
+    }
+
+}
